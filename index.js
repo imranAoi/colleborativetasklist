ae373d53b411c14e9c2dd4f4d94c642b8ff7ea12
sahil <mohdsahil.ds21@satyug.edu.in>
2025-08-05 00:57:58 +0530

    new 3

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ app.use(cors({
   },
   credentials: true
 }));
+app.options('*', cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(cookieParser());
 app.use('/api',AuthRouter)
 app.get('/',(req,res)=>{
