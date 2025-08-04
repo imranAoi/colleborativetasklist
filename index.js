403bf1b0b76c77427624be2f873ec05003144159
sahil <mohdsahil.ds21@satyug.edu.in>
2025-08-04 23:45:56 +0530

    new 2

--- index.js
+++ index.js
@@ -10,10 +10,21 @@ dotenv.config()
 const app=express()
 const PORT = "8000"
 app.use(express.json())
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://frontendof-ctl.vercel.app"
+];
+
 app.use(cors({
-    origin:"http://localhost:3000",
-    credentials:true
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 app.use(cookieParser());
 app.use('/api',AuthRouter)
 app.get('/',(req,res)=>{
